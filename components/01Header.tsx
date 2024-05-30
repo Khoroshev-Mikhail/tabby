5cf60df1101cf686d8368f2d708d47ea4da4c6a3
Mikha38 <oyks1379@yandex.ru>
2024-05-30 16:19:01 +0300

    Первый коммит

---
+++ components/01Header.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Header(){
+    return(
+        <section className="_section">
+            <div className="_wrapper">
+
+            </div>
+        </section>
+    )
+}
